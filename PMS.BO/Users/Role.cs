f99c2083ece88c34e74a82f188ccce23a1df79ec
chapal <chapal@celimited.com>
2025-01-25 16:08:20 +0600

    User Role

---
+++ PMS.BO/Users/Role.cs
@@ -0,0 +1,44 @@
+﻿using PMS.MODEL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+
+namespace PMS.BO
+{
+    public class Role : BasicBase
+    {
+        #region Constructor
+
+        public Role()
+        {
+            Code = string.Empty;
+            Name = string.Empty;
+
+        }
+
+        #endregion
+
+        #region Properties
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string Description { get; set; }
+    
+    
+        #endregion
+    }
+
+   
+    public interface IRoleService
+    {
+        Role Get(int id);
+        List<Role> GetAllRole();
+        int Save(Role item);
+        void UpdateRoleStatus(Role item);
+        void Delete(int id);
+     
+
+    }
+
+}
---
+++ PMS.BO/Users/UserRole.cs
@@ -0,0 +1,53 @@
+﻿using PMS.MODEL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace PMS.BO
+{
+    #region UserRole
+
+    public class UserRole : BasicBase
+    {
+        #region Constructor
+
+        public UserRole()
+        {
+      
+        }
+
+        #endregion
+
+        #region Properties
+        public int Id { get; set; }
+        public int RoleID { get; set; }
+        public int UserID { get; set; }
+
+        // Navigation Properties
+        public Role Role { get; set; } = new Role();
+        public User User { get; set; } = new User();
+       
+        
+
+        #endregion
+
+
+    }
+    #endregion
+
+    #region IUserRole Service
+
+    public interface IUserRoleService
+    {
+        UserRole Get(int id);
+
+        List<UserRole> GetByUserID(int UserID);
+
+        DataTable GetUserMenuByRole(string roleId);
+    
+        void Save(List<UserRole> userRoles, int payrolltypeid);
+        void Delete(int id);
+    }
+
+    #endregion
+}
